ba92dec7e8bb82ed7be1d9adea77fbdac506a5f8
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-08-16 10:20:35 +1200

    relative path

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -6,11 +6,13 @@ load_dotenv()
 
 openai.api_key = os.getenv('API_KEY')
 
-if not os.path.exists('prompt.txt'):
+path = os.path.join(os.path.dirname(__file__), "prompt.txt")
+
+if not os.path.exists(path):
     raise Exception('No prompt.txt file found')
 
 """Enter custom system prompt to change the bot's personality"""
-with open('prompt.txt', 'r') as file:
+with open(path, 'r') as file:
     SYSTEM_PROMPT = file.read().replace('\n', ' ')
 
 def chatgpt_response(prompt: str, memory):
